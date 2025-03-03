1155aa735926ce16a576c37c87cd5cb2941dc6aa
ninetailsrabbit <danieldigitalspace@proton.me>
2025-03-03 14:12:05 +0000

    renamed plugin settings to avoid conflicts when using more addons at the same time

--- addons/ninetailsrabbit.indie_blueprint_rpg/settings/plugin_settings.gd
+++ addons/ninetailsrabbit.indie_blueprint_rpg/settings/plugin_settings.gd
@@ -1,5 +1,5 @@
 @tool
-class_name MyPluginSettings extends RefCounted
+class_name IndieBlueprintRpgSettings extends RefCounted
 
 const PluginPrefixName: String = "ninetailsrabbit.indie_blueprint_rpg" ## The folder name
 const GitRepositoryName: String = "indie-blueprint-rpg"
